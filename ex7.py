003456bb3fe51c23d94c9658c7a5d32be7e03da0
obub <bubenshchikov@iskrauraltel.ru>
2021-06-24 15:04:53 +0500

    ex7

---
+++ ex7.py
@@ -0,0 +1,37 @@
+"""
+Ex7
+"""
+import requests
+
+
+api_url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+
+print("--- case 1. ---")
+response = requests.get(api_url)
+print("The response is:", response.text)
+
+print("--- case 2. ---")
+response = requests.post(api_url, data={'method': "HEAD"})
+print("The response is:", response.text)
+
+print("--- case 3. ---")
+response = requests.put(api_url, data={'method': "PUT"})
+print("The response is:", response.text)
+
+print("--- case 4. ---")
+methods_name = {"GET": lambda rq, url, data: rq.get(url, params=data),
+                "POST": lambda rq, url, data: rq.post(url, data=data),
+                "PUT": lambda rq, url, data: rq.put(url, data=data),
+                "DELETE": lambda rq, url, data: rq.delete(url, data=data)}
+
+for req_name, req_method in methods_name.items():
+    for send_method in methods_name:
+        if req_name == "GET":
+            response = req_method(requests, api_url, {'method': send_method})
+        else:
+            response = req_method(requests, api_url, {'method': send_method})
+
+        print(f"req_method: {req_name}, data: {send_method}, response text: {response.text}, status: {response.status_code}")
+
+        if req_name != send_method and '{"success":"!"}' in response.text:
+            print(f"Wrong case: request method={req_name}, data={send_method}, answer='{response.text}' ")
